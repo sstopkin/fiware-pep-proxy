5927ac27f173c1c4fa146c554a9f59086300713d
Sergey Stopkin <stopkin.sergey@gmail.com>
2018-05-18 13:15:20 +0600

    Updated entrypoint

--- extras/docker/docker-entrypoint.sh
+++ extras/docker/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 source entrypoint-common.sh
 
-check_url "http://idm:3000" ".*"
-check_url "http://authzforce:8080" ".*"
-check_url "http://orion:1026" ".*"
+check_url "http://${IDM_URL:-idm}:3000" ".*"
+check_url "http://${AUTHZFORCE_URL:-authzforce}:8080" ".*"
+check_url "http://${ORION_URL:-orion}:1026" ".*"
 
 node server.js
