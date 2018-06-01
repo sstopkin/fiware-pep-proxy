bcc45b841f8b838cd2fe6fcf0cae7c2c6f356cec
Sergey Stopkin <stopkin.sergey@gmail.com>
2018-06-01 22:53:29 +0600

    Customized target service

--- extras/docker/docker-entrypoint.sh
+++ extras/docker/docker-entrypoint.sh
@@ -4,6 +4,6 @@ source entrypoint-common.sh
 
 check_url "http://${IDM_URL:-idm}:3000" ".*"
 check_url "http://${AUTHZFORCE_URL:-authzforce}:8080" ".*"
-check_url "http://${ORION_URL:-orion}:1026" ".*"
+check_url "http://${SERVICE_URL}:${SERVICE_PORT}" ".*"
 
 node server.js
